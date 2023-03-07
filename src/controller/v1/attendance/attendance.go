95850bb8fc076d9afc651d6426281ac5ec316a28
Mochamad Rafli Ramadhan <rafliugm222@gmail.com>
2023-03-07 09:50:29 +0700

    feat: add filter year and update return time in UTC+7 :rocket:

--- src/controller/v1/attendance/attendance.go
+++ src/controller/v1/attendance/attendance.go
@@ -5,15 +5,16 @@ import (
 	"net/http"
 	"strconv"
 
-	echo "github.com/labstack/echo/v4"
-	entity "go-rest-api/src/http"
-	"github.com/forkyid/go-utils/v1/validation"
-	"github.com/pkg/errors"
 	"go-rest-api/src/constant"
+	entity "go-rest-api/src/http"
 	"go-rest-api/src/pkg/jwt"
 	"go-rest-api/src/pkg/pagination"
-	"go-rest-api/src/pkg/rest"
 	"go-rest-api/src/service/v1/attendance"
+
+	"github.com/forkyid/go-utils/v1/rest"
+	"github.com/forkyid/go-utils/v1/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 )
 
 type Controller struct {
@@ -33,22 +34,22 @@ func NewController(
 // @Tags Attendance 
 // @Produce application/json
 // @Param Authorization header string true "Bearer Token"
-// @Param Limit query string true "limit"
 // @Param Page query string true "page"
-// @Param Filter query string true "string enums" Enums(day, week, month)
+// @Param Limit query string true "limit"
+// @Param Filter query string true "string enums" Enums(day, week, month, year)
 // @Success 200 {object} http.GetAttendance
 // @Failure 400 {string} string "Bad Request"
 // @Failure 401 {string} string "Unauthorized"
 // @Failure 500 {string} string "Internal Server Error"
 // @Router /v1/attendance/history [get]
-func (ctrl *Controller) Get(ctx echo.Context) {
-	accountID, err := jwt.ExtractID(ctx.Request().Header.Get("Authorization"))
+func (ctrl *Controller) Get(ctx *gin.Context) {
+	accountID, err := jwt.ExtractID(ctx.GetHeader("Authorization"))
 	if err != nil {
 		rest.ResponseMessage(ctx, http.StatusUnauthorized)
 		return
 	}
 
-	limitString := ctx.QueryParam("Limit")
+	limitString := ctx.Query("Limit")
 	limit, err := strconv.Atoi(limitString)
 	if err != nil {
 		log.Print(err)
@@ -57,7 +58,7 @@ func (ctrl *Controller) Get(ctx echo.Context) {
 		return
 	}
 	limit = int(limit)
-	pageString := ctx.QueryParam("Page")
+	pageString := ctx.Query("Page")
 	page, err := strconv.Atoi(pageString)
 	if err != nil {
 		log.Print(err)
@@ -82,9 +83,9 @@ func (ctrl *Controller) Get(ctx echo.Context) {
 	}
 	pgn.Paginate()
 
-	filter := ctx.QueryParam("Filter")
+	filter := ctx.Query("Filter")
 	log.Print(filter)
-	if filter != constant.FilterByDay && filter != constant.FilterByWeek && filter != constant.FilterByMonth {
+	if filter != constant.FilterByDay && filter != constant.FilterByWeek && filter != constant.FilterByMonth && filter != constant.FilterByYear {
 		rest.ResponseError(ctx, http.StatusBadRequest, map[string]string{
 			"filter": constant.ErrInvalidFormat.Error()})
 		return
@@ -110,21 +111,21 @@ func (ctrl *Controller) Get(ctx echo.Context) {
 // @Tags Attendance 
 // @Produce application/json
 // @Param Authorization header string true "Bearer Token"
-// @Param Limit query string true "limit"
 // @Param Page query string true "page"
+// @Param Limit query string true "limit"
 // @Success 200 {object} http.GetAttendanceByLocation
 // @Failure 400 {string} string "Bad Request"
 // @Failure 401 {string} string "Unauthorized"
 // @Failure 500 {string} string "Internal Server Error"
 // @Router /v1/attendance/locations [get]
-func (ctrl *Controller) GetByLocation(ctx echo.Context) {
-	accountID, err := jwt.ExtractID(ctx.Request().Header.Get("Authorization"))
+func (ctrl *Controller) GetByLocation(ctx *gin.Context) {
+	accountID, err := jwt.ExtractID(ctx.GetHeader("Authorization"))
 	if err != nil {
 		rest.ResponseMessage(ctx, http.StatusUnauthorized)
 		return
 	}
 
-	limitString := ctx.QueryParam("Limit")
+	limitString := ctx.Query("Limit")
 	limit, err := strconv.Atoi(limitString)
 	if err != nil {
 		log.Print(err)
@@ -133,7 +134,7 @@ func (ctrl *Controller) GetByLocation(ctx echo.Context) {
 		return
 	}
 	limit = int(limit)
-	pageString := ctx.QueryParam("Page")
+	pageString := ctx.Query("Page")
 	page, err := strconv.Atoi(pageString)
 	if err != nil {
 		log.Print(err)
@@ -176,22 +177,23 @@ func (ctrl *Controller) GetByLocation(ctx echo.Context) {
 // Create godoc
 // @Summary Add Attendance
 // @Description Add Attendance
-// @Tags Attendance 
+// @Tags Attendance
+// @Param Authorization header string true "Bearer Token"
 // @Param Payload body http.AddAttendance true "Payload"
 // @Success 201 {object} string "Created"
 // @Failure 400 {string} string "Bad Request"
 // @Failure 409 {string} string "Resource Conflict"
 // @Failure 500 {string} string "Internal Server Error"
 // @Router /v1/attendance [post]
-func (ctrl *Controller) Add(ctx echo.Context) {
-	accountID, err := jwt.ExtractID(ctx.Request().Header.Get("Authorization"))
+func (ctrl *Controller) Add(ctx *gin.Context) {
+	accountID, err := jwt.ExtractID(ctx.GetHeader("Authorization"))
 	if err != nil {
 		rest.ResponseMessage(ctx, http.StatusUnauthorized)
 		return
 	}
 
-	req := new(entity.AddAttendance)
-	if err := ctx.Bind(req); err != nil {
+	req := entity.AddAttendance{}
+	if err := rest.BindJSON(ctx, &req); err != nil {
 		rest.ResponseError(ctx, http.StatusBadRequest, map[string]string{
 			"body": constant.ErrInvalidFormat.Error()})
 		return
@@ -204,12 +206,16 @@ func (ctrl *Controller) Add(ctx echo.Context) {
 		return
 	}
 
-	request := *req
-	err = ctrl.svc.Add(accountID, request)
+	err = ctrl.svc.Add(accountID, req)
 	if errors.Is(err, constant.ErrAccountNotRegistered) {
 		rest.ResponseError(ctx, http.StatusBadRequest, map[string]string{
 			"account_id": constant.ErrAccountNotRegistered.Error()})
-		return
+	} else if errors.Is(err, constant.ErrLocationNotExist) {
+		rest.ResponseError(ctx, http.StatusBadRequest, map[string]string{
+			"location": constant.ErrLocationNotExist.Error()})
+	} else if errors.Is(err, constant.ErrInvalidStatusAttendance) {
+		rest.ResponseError(ctx, http.StatusBadRequest, map[string]string{
+			"status": constant.ErrInvalidStatusAttendance.Error()})
 	} else if err != nil {
 		log.Println("attendance name:", err.Error())
 		rest.ResponseMessage(ctx, http.StatusInternalServerError)
